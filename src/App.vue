6f4178663a6cc9a1b159d94ba4dd6decf2bfaeb6
Administrator <yangzheng@himobi.net>
2019-08-09 11:42:18 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+<!--    <img src="./assets/logo.png">-->
+<!--    <router-view/>-->
+<!--    <el-button type="success">1</el-button>-->
+<!--    <h1>site : {{site}}</h1>-->
+    <tree></tree>
+  </div>
+</template>
+
+<script>
+import tree from './component/tree/tree.vue'
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      site: '菜鸟教程'
+    }
+  },
+  components: {tree}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
